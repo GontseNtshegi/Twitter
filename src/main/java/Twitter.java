e2db4726af686003c620f32bbc2647fde82b032b
Gontse Ntshegi <Gontse.Ntshegi@standardbank.co.za>
2018-07-12 14:39:18 +0200

    sort tweet based on priority

--- src/main/java/Twitter.java
+++ src/main/java/Twitter.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Twitter {
     private static Map<Integer, User> users;
+    private static int priority=0;
 
     public static void main(String[] args){
 
@@ -17,10 +18,10 @@ public class Twitter {
         ward.addFollower(2);
         ward.addFollower(1);
 
-        alan.addTweet("If you have a procedure with 10 parameters, you probably missed some.");
-        ward.addTweet("There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.");
-        martin.addTweet("Random numbers should not be generated with a method chosen at random.");
-        martin.addTweet("If you have a procedure with 10 parameters, you probably missed some.");
+        alan.addTweet("If you have a procedure with 10 parameters, you probably missed some.", priority++);
+        ward.addTweet("There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.", priority++);
+        alan.addTweet("Random numbers should not be generated with a method chosen at random.", priority++);
+
 
         users = new HashMap<Integer, User>();
         users.put(0, ward);
@@ -33,7 +34,22 @@ public class Twitter {
     private static void showNewsFeed(){
         Map<Integer, User> sortUsers = sortUsersByName(users);
         for(Map.Entry<Integer, User> entry: sortUsers.entrySet()){
-            System.out.println("key: "+ entry.getKey() + " Value: "+ entry.getValue().name);
+            Set<Integer> followers = entry.getValue().getFollowers();
+            List<Tweet> allTweets = new ArrayList<Tweet>();
+            for(int follower: followers){
+                //System.out.println(sortUsers.get(follower).name);
+                List<Tweet> tweet = sortUsers.get(follower).getCurrentTweet();
+                allTweets.addAll(tweet);
+            }
+            Collections.sort(allTweets, new Comparator<Tweet>() {
+                public int compare(Tweet user1, Tweet user2) {
+                    return (user1.getPriority()+"").compareTo(user2.getPriority()+"");
+                }
+            });
+            System.out.println(entry.getValue().getName());
+            for(Tweet tweet: allTweets){
+                System.out.println("\t" +"@"+sortUsers.get(tweet.getUserId())+":" + tweet.getMessage());
+            }
         }
     }
     private static Map<Integer, User> sortUsersByName(Map<Integer, User> users){
@@ -42,7 +58,7 @@ public class Twitter {
 
         Collections.sort(list, new Comparator<Map.Entry<Integer, User>>() {
             public int compare(Map.Entry<Integer, User> user1, Map.Entry<Integer, User> user2) {
-                return (user1.getValue().name).compareTo(user2.getValue().name);
+                return (user1.getValue().getName()).compareTo(user2.getValue().getName());
             }
         });
 
