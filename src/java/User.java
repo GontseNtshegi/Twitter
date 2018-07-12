ec90429e271d57b90f89edf354f7c025eab75658
Gontse Ntshegi <Gontse.Ntshegi@standardbank.co.za>
2018-07-12 09:20:37 +0200

    added user class

--- src/java/User.java
+++ src/java/User.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import lombok.Data;
 
+import java.util.Set;
+
+/**
+ *
+ */
+@Data
 public class User {
+    public String name;
+    public Integer identifier;
+    public Set<Integer> followers;
+    public Tweet currentTweet;
 }
