7434f9fac97c2e0b9f1f6c08cae6cd10518f8fdd
Gontse Ntshegi <Gontse.Ntshegi@standardbank.co.za>
2018-07-12 14:38:57 +0200

    added priority to differentiate which tweet came first

--- src/main/java/Tweet.java
+++ src/main/java/Tweet.java
@@ -6,12 +6,16 @@ import lombok.Data;
 @Data
 public class Tweet {
  public String message;
- public Tweet nextTweet;
+ //public Tweet nextTweet;
+ public int priority;
+ public Integer userId;
 
- public Tweet(String message)
+ public Tweet(String message, int prio, Integer userid)
  {
   this.message = message;
-  this.nextTweet = null;
+  //this.nextTweet = null;
+  this.priority = prio;
+  this.userId = userid;
  }
 
 }
