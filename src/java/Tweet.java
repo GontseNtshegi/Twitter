43acddcccedb83849fc7297ea99e405d0fdba445
Gontse Ntshegi <Gontse.Ntshegi@standardbank.co.za>
2018-07-12 11:54:30 +0200

    added tweet functionality

--- src/java/Tweet.java
+++ src/java/Tweet.java
@@ -5,5 +5,13 @@ import lombok.Data;
  */
 @Data
 public class Tweet {
+ public String message;
+ public Tweet nextTweet;
+
+ public Tweet(String message)
+ {
+  this.message = message;
+  this.nextTweet = null;
+ }
 
 }
--- src/java/User.java
+++ src/java/User.java
@@ -11,4 +11,24 @@ public class User {
     public Integer identifier;
     public Set<Integer> followers;
     public Tweet currentTweet;
+
+    public User(String n, Integer id){
+        this.name = n;
+        this.identifier = id;
+        this.followers.add(id); //user can also follow himself
+        this.currentTweet = null;
+    }
+
+    public void addFollower(Integer identifier){
+        this.followers.add(identifier);
+    }
+    public void removeFollower(Integer identifier){
+        this.followers.remove(identifier);
+    }
+    public void addTweet(String message){
+        Tweet tweet = new Tweet(message);
+        tweet.nextTweet = currentTweet;
+        this.currentTweet = tweet;
+
+    }
 }
